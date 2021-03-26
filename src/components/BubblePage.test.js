5262e4f747870998e36461e06929fcabcfb8823c
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-26 13:49:42 -0400

    test are done and MVP is complete

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,41 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+
+import { fetchColors as mockFetchColors } from '../api/fetchColors'
 import BubblePage from "./BubblePage";
 
-test("Renders BubblePage without errors", () => {
+jest.mock("../api/fetchColors.js");
+
+const colorsData = [
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  },
+]
+
+test("Renders BubblePage without errors", async () => {
   // Finish this test
+  await mockFetchColors.mockResolvedValueOnce(colorsData);
+  render(<BubblePage />);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
+  mockFetchColors.mockResolvedValueOnce(colorsData);
+  render (<BubblePage />);
+
+  const colorBubbles = screen.findByText(/bubbles/i);
+  expect(await colorBubbles).toBeInTheDocument();
 });
 
 //Task List
